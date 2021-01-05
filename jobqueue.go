d7aea29f6f5b8817f3289065e3255e15c814388e
lkbhargav <lkbhargav9@gmail.com>
2021-01-05 15:15:25 -0500

    Gives more control for the end user

--- jobqueue.go
+++ jobqueue.go
@@ -1,8 +1,36 @@
 package jobqueue
 
 import (
+	"context"
 	"math/rand"
+	"sync"
 	"time"
+)
+
+// Priority => holds the priority types
+type Priority int
+
+const (
+	// High => absolutely important
+	High Priority = 1
+	// Medium => Kind of important
+	Medium Priority = 2
+	// Low => Not that important
+	Low Priority = 3
+)
+
+// Status => holds the status types
+type Status string
+
+const (
+	// Running => signifies a job that is running
+	Running Status = "Running"
+	// Completed => signifies job completeion
+	Completed Status = "Completed!"
+	// Killed => signifies job is killed
+	Killed Status = "Killed!"
+	// InQueue => signifies job is in queue
+	InQueue Status = "In queue"
 )
 
 // Master => master instance that gets returned on Job queue initialization
@@ -19,18 +47,20 @@ type JobRelated struct {
 // JobRelatedData => contract that the caller has to provide details about the job | also dubs as status manager / for job log recording
 type JobRelatedData struct {
 	Description      string        // for our own reference
-	Priority         int           // three levels available | 1 => High; 2 => Medium; 3 => Low;
+	Priority         Priority      // three levels available | 1 => High; 2 => Medium; 3 => Low;
 	JobStatus        JobStatus     // struct implementing the interface that is core
 	Params           []interface{} // optional set of parameters
 	createdTime      time.Time
 	message          string
-	status           string
+	status           Status
 	statusChangeTime time.Time
+	channel          chan bool
+	completed        bool
 }
 
 // JobStatus => provides an interface for apps to implement and make it compatible with jobqueue
 type JobStatus interface {
-	Run(jr JobRelated, args ...interface{})
+	Run(ctx context.Context, jr JobRelated, args ...interface{})
 }
 
 // JobInfo => used to expose data to the user
@@ -39,15 +69,16 @@ type JobInfo struct {
 	Description string
 	Message     string
 	CreatedTime time.Time
-	Status      string
+	Status      Status
 	UpdatedTime time.Time
-	Priority    int
+	Priority    Priority
 }
 
 const characters string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 var jobs map[string]JobRelatedData
 var jobInProgress bool
+var jobsMutex sync.Mutex
 
 // Init => helps initialize
 func Init() Master {
@@ -66,21 +97,41 @@ func Init() Master {
 				outerLoop:
 					for priority := 1; priority <= 3; priority++ { // helps prioritize between tasks in hand based on priority
 						for jobID, v := range jobs { // loops through jobs until a fit is found as in the condition below
-							if v.Priority == priority {
+							if v.Priority == Priority(priority) && !v.completed {
 								go func() {
-									v.status = "Running"
+									v.status = Running
 									v.statusChangeTime = time.Now()
+									v.channel = make(chan bool, 1)
+
+									jobsMutex.Lock()
 									jobs[jobID] = v // updating the jobs map with updated values
+									jobsMutex.Unlock()
 
-									compute(v.JobStatus, JobRelated{jobID: jobID}, v.Params...) // running the job in question as requested by the user
+									compute(v.JobStatus, JobRelated{jobID: jobID}, v) // running the job in question as requested by the user
 
-									delete(jobs, jobID) // remove the job that just got completed executing
+									v.status = Completed
+									v.statusChangeTime = time.Now()
+									v.completed = true
+
+									jobsMutex.Lock()
+									jobs[jobID] = v // updating the jobs map with updated values
+									jobsMutex.Unlock()
+
+									go func() { // we will remove the job from the list only after 24 hours
+										time.Sleep(24 * time.Hour)
+										delete(jobs, jobID) // remove the job that just got completed executing
+									}()
 
 									jobInProgress = false // helps open gates for new jobs
 								}()
+
 								v.createdTime = time.Now()
 								jobInProgress = true
+
+								jobsMutex.Lock()
 								jobs[jobID] = v
+								jobsMutex.Unlock()
+
 								break outerLoop // breaks outerloop once it finds a job to assign to
 							}
 						}
@@ -93,19 +144,35 @@ func Init() Master {
 	return Master{jobs: jobs, done: done}
 }
 
-func compute(js JobStatus, jr JobRelated, args ...interface{}) { // The method that implements our interfaces | HERO
-	js.Run(jr, args...)
+func compute(js JobStatus, jr JobRelated, job JobRelatedData) { // The method that implements our interfaces | HERO
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		js.Run(ctx, jr, job.Params...)
+		job.channel <- true
+	}()
+
+	select {
+	case val := <-job.channel:
+		if val {
+			cancel()
+			return
+		}
+	}
+
+	cancel() // to avoid context leaks
 }
 
 // Add => helps add a job
 func (m Master) Add(job JobRelatedData) JobRelated {
 	jobID := randomPrefix(10)
 
-	job.status = "In queue"
+	job.status = InQueue
 	job.createdTime = time.Now()
 	job.statusChangeTime = time.Now()
 
+	jobsMutex.Lock()
 	m.jobs[jobID] = job
+	jobsMutex.Unlock()
 
 	return JobRelated{jobID: jobID}
 }
@@ -134,16 +201,63 @@ func (m Master) GetListOfJobs() (response []JobInfo) {
 	return
 }
 
-// Kill => acts like a kill switch to put down the jobs queue service (mostly the go routine that runs in the background)
-func (m Master) Kill() {
+// EndJobQueue => acts like a kill switch to put down the jobs queue service (mostly the go routine that runs in the background)
+func (m Master) EndJobQueue() {
 	m.done <- true
+
+	for jobID, job := range m.jobs {
+		if job.status == Running {
+			job.channel <- true
+		}
+		job.completed = true
+		job.status = Killed
+
+		jobsMutex.Lock()
+		jobs[jobID] = job
+		jobsMutex.Unlock()
+	}
+
+	go func() {
+		time.Sleep(10 * time.Second)
+		m.jobs = map[string]JobRelatedData{}
+	}()
 }
 
 // Update => lets us update the status of the job periodically
 func (jr JobRelated) Update(message string) {
 	job := jobs[jr.jobID]
 	job.message = message
+
+	jobsMutex.Lock()
 	jobs[jr.jobID] = job
+	jobsMutex.Unlock()
+}
+
+// Kill => kills a job
+func (m Master) Kill(jobID string) (resp bool) {
+	job := m.jobs[jobID]
+
+	if job.Priority == 0 { // filtering invalid jobID's
+		return
+	}
+
+	if !job.completed && job.status == Running {
+		job.channel <- true
+		job.status = Killed
+		job.statusChangeTime = time.Now()
+		resp = true
+	} else if !job.completed && job.status != Running {
+		job.completed = true
+		job.status = Killed
+		job.statusChangeTime = time.Now()
+		resp = true
+	}
+
+	jobsMutex.Lock()
+	m.jobs[jobID] = job
+	jobsMutex.Unlock()
+
+	return
 }
 
 func randomPrefix(n int) string { // generates random job ids on the fly
--- jobqueue_test.go
+++ jobqueue_test.go
@@ -1,40 +1,138 @@
 package jobqueue
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"testing"
 	"time"
 )
 
 type asyncJob struct {
-	name string
+	name    string
+	timeout int
 }
 
 // Run => implementing the jobqueue interface method
-func (aJob asyncJob) Run(jr JobRelated, args ...interface{}) {
-	fmt.Println(aJob.name, args[1])
-	jr.Update("Printed a message")
-	time.Sleep(5 * time.Second) // for simulating some intense work
+func (aJob asyncJob) Run(ctx context.Context, jr JobRelated, args ...interface{}) {
+	defer ctx.Done()
+	fmt.Println(args[0], args[1])
+	jr.Update(args[0].(string))
+	time.Sleep(time.Duration(aJob.timeout) * time.Second) // for simulating some intense work
+}
+
+func printJobList(jobsList []JobInfo) {
+	js, _ := json.Marshal(jobsList)
+	fmt.Println(string(js))
 }
 
 func TestJobsList(t *testing.T) {
 	var aJob asyncJob
 
-	aJob.name = "Bhargav"
+	aJob.name = "Test job"
+	aJob.timeout = 5
 
 	jqueue := Init()
-	defer jqueue.Kill()
+	defer jqueue.EndJobQueue()
 
-	jqueue.Add(JobRelatedData{Description: "Test 1", Priority: 3, JobStatus: aJob, Params: []interface{}{"name", "anything else 1"}})
-	jqueue.Add(JobRelatedData{Description: "Test 2", Priority: 1, JobStatus: aJob, Params: []interface{}{"name", "anything else 2"}})
-	jqueue.Add(JobRelatedData{Description: "Test 3", Priority: 2, JobStatus: aJob, Params: []interface{}{"name", "anything else 3"}})
-	jqueue.Add(JobRelatedData{Description: "Test 4", Priority: 2, JobStatus: aJob, Params: []interface{}{"name", "anything else 4"}})
+	jqueue.Add(JobRelatedData{Description: "Test 1", Priority: Low, JobStatus: aJob, Params: []interface{}{"Yudhistira", "anything else 1"}})
+	jqueue.Add(JobRelatedData{Description: "Test 2", Priority: High, JobStatus: aJob, Params: []interface{}{"Arjuna", "anything else 2"}})
+	jqueue.Add(JobRelatedData{Description: "Test 3", Priority: Medium, JobStatus: aJob, Params: []interface{}{"Bhima", "anything else 3"}})
+	jqueue.Add(JobRelatedData{Description: "Test 4", Priority: Medium, JobStatus: aJob, Params: []interface{}{"Sahadeva", "anything else 4"}})
+	jqueue.Add(JobRelatedData{Description: "Test 5", Priority: High, JobStatus: aJob, Params: []interface{}{"Nakula", "anything else 5"}})
 
-	josbList := jqueue.GetListOfJobs()
+	jobsList := jqueue.GetListOfJobs()
 
-	time.Sleep(30 * time.Second)
+	var jobID string
 
-	if len(josbList) != 4 {
-		t.Errorf("Expected to see just one job, but instead found %v jobs", len(josbList))
+	for _, job := range jobsList {
+		if job.Description == "Test 1" {
+			jobID = job.JobID
+		}
+	}
+
+	resp := jqueue.Kill(jobID)
+
+	if !resp {
+		t.Errorf("Expected the value to be true instead found it to %v", resp)
+	}
+
+	time.Sleep(35 * time.Second)
+
+	jobs := jqueue.GetListOfJobs()
+
+	if len(jobsList) != 5 {
+		t.Errorf("Expected to see just one job, but instead found %v jobs", len(jobsList))
+	}
+
+	for _, j := range jobs {
+		if j.JobID == jobID {
+			if j.Status != Killed {
+				t.Errorf("Expected the job to be killed but instead found it to be %v", j.Status)
+			}
+		}
+	}
+}
+
+func TestForKillingARunningJob(t *testing.T) {
+	var aJob asyncJob
+
+	aJob.name = "Test job 2"
+	aJob.timeout = 20
+
+	jqueue := Init()
+	defer jqueue.EndJobQueue()
+
+	jqueue.Add(JobRelatedData{Description: "Test 1", Priority: Low, JobStatus: aJob, Params: []interface{}{"Yudhistira", "anything else 1"}})
+
+	time.Sleep(2 * time.Second)
+
+	resp := jqueue.Kill(jqueue.GetListOfJobs()[0].JobID)
+
+	if !resp {
+		t.Errorf("Expected the value to be true instead found it to %v", resp)
+	}
+}
+
+func TestForKillingAInvalidJob(t *testing.T) {
+	var aJob asyncJob
+
+	aJob.name = "Test job 3"
+	aJob.timeout = 20
+
+	jqueue := Init()
+	defer jqueue.EndJobQueue()
+
+	jqueue.Add(JobRelatedData{Description: "Test 1", Priority: Low, JobStatus: aJob, Params: []interface{}{"Yudhistira", "anything else 1"}})
+
+	time.Sleep(2 * time.Second)
+
+	resp := jqueue.Kill("abcd")
+
+	if resp {
+		t.Errorf("Expected the value to be false instead found it to %v", resp)
+	}
+}
+
+func TestForAfterKillingJobQueue(t *testing.T) {
+	var aJob asyncJob
+
+	aJob.name = "Test job 3"
+	aJob.timeout = 20
+
+	jqueue := Init()
+
+	jqueue.Add(JobRelatedData{Description: "Test 1", Priority: Low, JobStatus: aJob, Params: []interface{}{"Yudhistira", "anything else 1"}})
+	jqueue.Add(JobRelatedData{Description: "Test 2", Priority: High, JobStatus: aJob, Params: []interface{}{"Arjuna", "anything else 2"}})
+	jqueue.Add(JobRelatedData{Description: "Test 3", Priority: Medium, JobStatus: aJob, Params: []interface{}{"Bhima", "anything else 3"}})
+	jqueue.Add(JobRelatedData{Description: "Test 4", Priority: Medium, JobStatus: aJob, Params: []interface{}{"Sahadeva", "anything else 4"}})
+	jqueue.Add(JobRelatedData{Description: "Test 5", Priority: High, JobStatus: aJob, Params: []interface{}{"Nakula", "anything else 5"}})
+
+	jqueue.EndJobQueue()
+
+	for _, job := range jobs {
+		if job.status != Killed || !job.completed {
+			t.Errorf("Expected the job to be killed, but found otherwise (status: %v, completed: %v)", job.status, job.completed)
+		}
 	}
 }
